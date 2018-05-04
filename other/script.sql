9b93695f29c43fda156814b0d4c6e67915e00ea3
卖黄瓜的小花菜 <zhangwenl1993@163.com>
2018-05-04 16:31:06 +0800

    修改数据库脚本

--- other/script.sql
+++ other/script.sql
@@ -11,3 +11,15 @@ CREATE TABLE traced_uid
   status     INT DEFAULT '0' NOT NULL
   COMMENT '0为正常订阅，1为暂停'
 ) CHARACTER SET UTF8;
+
+CREATE TABLE email_log
+(
+  id             INT(10) AUTO_INCREMENT
+    PRIMARY KEY,
+  from_mail      VARCHAR(250) NULL,
+  to_mail        VARCHAR(250) NULL,
+  user_id        INT(10)      NULL,
+  send_timestamp VARCHAR(25)  NULL,
+  context        MEDIUMTEXT   NULL,
+  weibo_count    INT(12)      NULL
+) CHARACTER SET UTF8;
